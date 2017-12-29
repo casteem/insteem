2872187f9cae533e1b2670c2c2dbc80ce9e4d797
sarasate <luis@superlumen.io>
2017-12-29 23:56:54 +0100

    Add .env file

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import "./lib/semantic/semantic.min.css";
+import "lib/semantic/semantic.min.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 
