041b5c8b117d0f71ebd3218ad110333ad78a15e0
Luis Höfer <luis@superlumen.io>
2018-01-18 13:43:59 +0100

    Setup apollo client and provider

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
 import "./App.css";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { ApolloProvider } from "react-apollo";
+import client from "services/graphql/client";
 import { Container, Grid, Segment, Header, Label } from "semantic-ui-react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import StoriesScene from "scenes/StoriesScene";
 import StoryScene from "scenes/StoryScene";
@@ -13,47 +15,49 @@ import ProfileScene from "scenes/Profile/ProfileScene";
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-          <MainMenu />
-          <CategoryMenu />
-          <Container>
-            <Grid>
-              <Grid.Column width={10}>
-                <Route exact path="/@:author" component={ProfileScene} />
-                <Route exact path="/" component={StoriesScene} />
-                <Route
-                  exact
-                  path="/categories/:category"
-                  component={CategoryScene}
-                />
-                <Route
-                  exact
-                  path="/stories/:author/:permlink"
-                  component={StoryScene}
-                />
-              </Grid.Column>
-              <Grid.Column width={6}>
-                <Header attached="top">How to</Header>
-                <Segment attached>
-                  <ul>
-                    <li>Go to steemit.com</li>
-                    <li>Create a new post</li>
-                    <li>
-                      Use{" "}
-                      <Label color="blue" basic>
-                        news
-                      </Label>{" "}
-                      as the first tag for now
-                    </li>
-                    <li>Use at least one of the tags from the main menu</li>
-                  </ul>
-                </Segment>
-              </Grid.Column>
-            </Grid>
-          </Container>
-        </div>
-      </Router>
+      <ApolloProvider client={client}>
+        <Router>
+          <div className="App">
+            <MainMenu />
+            <CategoryMenu />
+            <Container>
+              <Grid>
+                <Grid.Column width={10}>
+                  <Route exact path="/@:author" component={ProfileScene} />
+                  <Route exact path="/" component={StoriesScene} />
+                  <Route
+                    exact
+                    path="/categories/:category"
+                    component={CategoryScene}
+                  />
+                  <Route
+                    exact
+                    path="/stories/:author/:permlink"
+                    component={StoryScene}
+                  />
+                </Grid.Column>
+                <Grid.Column width={6}>
+                  <Header attached="top">How to</Header>
+                  <Segment attached>
+                    <ul>
+                      <li>Go to steemit.com</li>
+                      <li>Create a new post</li>
+                      <li>
+                        Use{" "}
+                        <Label color="blue" basic>
+                          news
+                        </Label>{" "}
+                        as the first tag for now
+                      </li>
+                      <li>Use at least one of the tags from the main menu</li>
+                    </ul>
+                  </Segment>
+                </Grid.Column>
+              </Grid>
+            </Container>
+          </div>
+        </Router>
+      </ApolloProvider>
     );
   }
 }
