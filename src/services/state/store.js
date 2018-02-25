5b39eff4e55f6722e6c6ba8fd6c55f7af3a32377
sarasate <luis@superlumen.io>
2018-02-25 21:01:44 +0100

    Add redux logger

--- src/services/state/store.js
+++ src/services/state/store.js
@@ -1,18 +1,27 @@
-import { createStore } from 'redux'
-import { persistStore, persistReducer } from 'redux-persist'
-import storage from 'redux-persist/lib/storage'
+import { createStore, applyMiddleware } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { logger } from "redux-logger";
 
-import rootReducer from './reducers'
+import rootReducer from "./reducers";
+
+const middlewares = [];
 
 const persistConfig = {
-  key: 'root',
-  storage: storage,
+  key: "root",
+  storage: storage
+};
+
+if (process.env.NODE_ENV === `development`) {
+  const { logger } = require(`redux-logger`);
+
+  middlewares.push(logger);
 }
 
-const persistedReducer = persistReducer(persistConfig, rootReducer)
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export default () => {
-  let store = createStore(persistedReducer)
-  let persistor = persistStore(store)
-  return { store, persistor }
-}
+  let store = createStore(persistedReducer, applyMiddleware(...middlewares));
+  let persistor = persistStore(store);
+  return { store, persistor };
+};
