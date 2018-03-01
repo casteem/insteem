8f01a7b955e26bf9506d5de8b32860e989d110dc
sarasate <luis@superlumen.io>
2018-03-01 09:42:00 +0100

    Add journalist

--- src/scenes/Journalists/JournalistsScene/JournalistsScene.js
+++ src/scenes/Journalists/JournalistsScene/JournalistsScene.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Header, List, Button } from "semantic-ui-react";
+import { Grid, Header, Label, Item, Button } from "semantic-ui-react";
 import { map } from "ramda";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
@@ -16,19 +16,18 @@ const journalists = [
     tags: ["politics", "wikileaks"]
   },
   {
+    name: "fedescholari",
+    desc:
+      "Italian (?) writer covering business, crypto" +
+      " and money related topics ",
+    tags: ["cryptocurrency", "money", "china"]
+  },
+  {
     name: "joshsigurdson",
     desc:
       'Journalist at World Alternative Media. Does video" +\n' +
       '    " interviews on DTube, talks about money, crypto and business.',
     tags: ["business", "politics", "cryptocurrency"]
-  },
-
-  {
-    name: "patricklancaster",
-    desc:
-      "Video and photo journalists. Reports exclusively about the war in" +
-      " Ukrain",
-    tags: ["politics", "war", "ukraine"]
   },
   {
     name: "news2share",
@@ -36,6 +35,20 @@ const journalists = [
       "An online media team from Washington. Range from" +
       "  local, national to international politics",
     tags: ["politics"]
+  },
+  {
+    name: "occupation",
+    desc:
+      "Writes about everything that comes to his mind," +
+      " gaming, conspiracy, photography, health and so on.",
+    tags: ["politics", "geoengineering", "conspiracy"]
+  },
+  {
+    name: "patricklancaster",
+    desc:
+      "Video and photo journalists. Reports exclusively about the war in" +
+      " Ukrain",
+    tags: ["politics", "war", "ukraine"]
   },
   {
     name: "cyberwarrior",
@@ -46,35 +59,55 @@ const journalists = [
   }
 ];
 
+const blockedJournalists = [
+  {
+    name: "sarahlouise",
+    desc:
+      "Posts daily news about anything. The level of english in the" +
+      " articles doesn't fit with the one in the comments and e.g. the" +
+      " `introduceyourself` post. No plagiarism detected so far.",
+    tags: []
+  }
+];
+
+const renderTags = map(label => <Label color="green">{label}</Label>);
+
 const renderJournalists = map(user => (
-  <List.Item>
-    <List.Content floated="right">
-      <Button
-        size="tiny"
-        color="blue"
-        as="a"
-        href={`https://steemit.com/@${user.name}`}
-        target="_blank"
-      >
-        Profile on Steemit
-      </Button>
-    </List.Content>
-    <List.Content>
-      <List.Header as={Link} to={`/@${user.name}`}>
+  <Item>
+    <Item.Content>
+      <Item.Header as={Link} to={`/@${user.name}`}>
         {user.name}
-      </List.Header>
-      <List.Description>{user.desc}</List.Description>
-    </List.Content>
-  </List.Item>
+      </Item.Header>
+      <Item.Description>{user.desc}</Item.Description>
+      <Item.Extra>
+        <Button
+          floated="right"
+          size="tiny"
+          color="blue"
+          as="a"
+          href={`https://steemit.com/@${user.name}`}
+          target="_blank"
+        >
+          Profile on Steemit
+        </Button>
+        <Label.Group size="mini">{renderTags(user.tags)}</Label.Group>
+      </Item.Extra>
+    </Item.Content>
+  </Item>
 ));
 
 const JournalistsScene = () => {
   return (
-    <div>
-      <Header>Journalists on Steem</Header>
+    <Grid centered columns={1}>
+      <Grid.Column>
+        <Header>Journalists on Steem</Header>
 
-      <List size="big">{renderJournalists(journalists)}</List>
-    </div>
+        <Item.Group divided>{renderJournalists(journalists)}</Item.Group>
+
+        <Header>Unapproved Journalists </Header>
+        <Item.Group divided>{renderJournalists(blockedJournalists)}</Item.Group>
+      </Grid.Column>
+    </Grid>
   );
 };
 
