6782f47548f0c231f74162d8663aa8cc71f4105a
sarasate <luis@superlumen.io>
2018-03-18 20:50:12 +0100

    Update story list item layout

--- src/components/StoryList/components/StoryListItem/StoryListItem.js
+++ src/components/StoryList/components/StoryListItem/StoryListItem.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { propOr, head } from "ramda";
 import { parseMetadata } from "services/helpers/format";
-import { Link } from "react-router-dom";
-import { Item } from "semantic-ui-react";
+import Link from "components/Style/Link";
+import styled from "styled-components";
 import removeMarkdown from "remove-markdown";
 
 import Body from "./components/StoryListItemBody";
-import Extra from "./components/StoryListItemExtra";
+import StoryExtra from "./components/StoryListItemExtra";
 
 const StoryListItem = props => {
   const { story } = props;
@@ -14,26 +14,55 @@ const StoryListItem = props => {
   const images = propOr([], "image")(parseMetadata(story.json_metadata));
   const image = head(images);
 
+  const Story = styled.div`
+    background-color: white;
+    border: 1px solid #d4d4d5;
+    margin-bottom: 1rem;
+    max-width: 700px;
+  `;
+  const Header = styled.h2`
+    padding: 1rem 1rem 0 1rem;
+    color: darkred;
+    font-weight: bold;
+  `;
+  const Image = styled.div`
+    width: 100%; /*or 70%, or what you want*/
+    height: 400px; /*or 70%, or what you want*/
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-image: url(${({ src }) => src});
+  `;
+  const Text = styled.div`
+    padding: 1rem;
+  `;
+  const Extra = styled.div`
+    padding: 0 1rem 1rem 1rem;
+  `;
+
   // Convert story.body (markdown or html) to pure text.
   return (
-    <Item>
-      {image ? <Item.Image src={image} /> : null}
-      <Item.Content>
-        <Item.Header
-          as={Link}
-          to={`/stories/${story.author}/${story.permlink}`}
-        >
+    <Story>
+      <Header>
+        <Link to={`/stories/${story.author}/${story.permlink}`}>
           {story.title}
-        </Item.Header>
+        </Link>
+      </Header>
+      {image ? (
+        <Link to={`/stories/${story.author}/${story.permlink}`}>
+          <Image src={image} />{" "}
+        </Link>
+      ) : null}
 
-        <Item.Description style={{ fontSize: "1.1rem", marginBottom: "1rem" }}>
-          <Body body={story.body} />
-        </Item.Description>
+      <Text style={{ fontSize: "1.1rem", marginBottom: "1rem" }}>
+        <Body body={story.body} />
+      </Text>
 
-        {/*<Meta story={story} />*/}
-        <Extra story={story} />
-      </Item.Content>
-    </Item>
+      {/*<Meta story={story} />*/}
+      <Extra>
+        <StoryExtra story={story} />
+      </Extra>
+    </Story>
   );
 };
 
---
+++ src/components/Style/Link.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { Link as ReactLink } from "react-router-dom";
+import styled from "styled-components";
+
+const Link = styled(ReactLink)`
+  color: darkred;
+`;
+
+export default Link;
